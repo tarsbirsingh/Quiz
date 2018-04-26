db687a3a450eb6bd84c12f55d689183b2bd1ae85
Tarsbir Singh <tarsbirsingh@gmail.com>
2018-04-26 17:23:44 +0530

    change in layout file Add radiobutton and EditText dimens files add strings files add All questions

--- app/src/main/java/com/example/hp/quiz/MainActivity.java
+++ app/src/main/java/com/example/hp/quiz/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.hp.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+
+       final EditText editName =  findViewById(R.id.editText_name);
+       final TextView displayText = findViewById(R.id.textView_name);
+       Button showButton = findViewById(R.id.button_show);
+
+       showButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String name = editName.getText().toString();
+               displayText.setText(name);
+
+           }
+       });
+   }
 }
