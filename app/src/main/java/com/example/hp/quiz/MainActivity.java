965a9d7a5ee98fb7c6f0061b2a4fbcf0c8b93b68
Tarsbir Singh <tarsbirsingh@gmail.com>
2018-04-28 15:06:07 +0530

    Add more Radio Button

--- app/src/main/java/com/example/hp/quiz/MainActivity.java
+++ app/src/main/java/com/example/hp/quiz/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.hp.quiz;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +9,12 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private RadioGroup radioGroup1;
+    RadioGroup radioGroup1;
+    RadioGroup radioGroup2;
+   
+    //RadioButton Define here with numeric series
     RadioButton radioButton1;
+    RadioButton radioButton2;
     int score=0;
    @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
        final EditText editName =  findViewById(R.id.editText_name);
        final TextView displayText = findViewById(R.id.textView_name);
        radioGroup1 = findViewById(R.id.radioGroup_Q1);
+       radioGroup2 = findViewById(R.id.radioGroup_Q2);
        Button showButton = findViewById(R.id.button_show);
 
        showButton.setOnClickListener(new View.OnClickListener() {
@@ -28,11 +32,16 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View v) {
                String name = editName.getText().toString();
                //radio button
-               int selectedId=radioGroup1.getCheckedRadioButtonId();
+               int selectedId1=radioGroup1.getCheckedRadioButtonId();
                radioButton1=findViewById(R.id.radioButton_Q1_op1);
+               int selectedId2=radioGroup1.getCheckedRadioButtonId();
+               radioButton2=findViewById(R.id.radioButton_Q2_op1);
                //if statement for radio button
-               if (selectedId==R.id.radioButton_Q1_op2) {
+               if (selectedId1==R.id.radioButton_Q1_op1) {
                     score +=1;
+               }
+               else if (selectedId2==R.id.radioButton_Q2_op4) {
+                   score +=1;
                }
                String hell ="Thanks"+name+"your score is"+score;
                //display value on screen
